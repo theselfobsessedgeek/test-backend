c4340ed58258b5958c23dc5ed7fd88ec8eea59d4
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2024-05-31 14:14:59 +0530

    Probably fixed it

--- kapsite.py
+++ kapsite.py
@@ -1,13 +1,13 @@
-#from . import create_app, g#, db, request
-#from flask_mongoengine import MongoEngine
+from . import create_app, g#, db, request
+from flask_mongoengine import MongoEngine
 # Start development web server
-#if __name__ == '__main__':
-#    app = create_app()
-#    def get_db():
-#        if 'db' not in g:
-#            g.db = MongoEngine()
-#            g.db.init_app(app)
-#        return g.db   
+if __name__ == '__main__':
+   app = create_app()
+   def get_db():
+       if 'db' not in g:
+           g.db = MongoEngine()
+           g.db.init_app(app)
+       return g.db   
     # @app.before_request
     # def before_request():
     #     g.db = db.init_app(app) #connect_db()
@@ -16,17 +16,17 @@
     #     db = g.pop('db', None)
     #     if db is not None:
     #         db.close()
- #   app.run(host='0.0.0.0', port=8000, debug=False, ssl_context=('cert.pem', 'key.pem'))
+   app.run(host='0.0.0.0', port=8000, debug=False, ssl_context=('cert.pem', 'key.pem'))
 
 
 
 
 
-from flask import Flask
-from __init__ import create_app 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return 'Hello world'
-if __name__ == '__main__':
-    app.run(debug=False, port=8000, host='0.0.0.0')
+# from flask import Flask
+# from __init__ import create_app 
+# app = Flask(__name__)
+# @app.route('/')
+# def index():
+#     return 'Hello world'
+# if __name__ == '__main__':
+#     app.run(debug=False, port=8000, host='0.0.0.0')
