27ef75bd1c2e283c9d6815fc88082a15988b7c50
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2024-06-20 01:57:39 +0530

    adding db config

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,10 @@ import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 sys.path.append(parent_dir)
 
-from .kapsite import app
+# from .kapsite import app
+
+import __init__ as init
 
 if __name__ == '__main__':
+    app = init.create_app()
     app.run(host='0.0.0.0', port=4000, debug=False, ssl_context=('cert.pem', 'key.pem'))
